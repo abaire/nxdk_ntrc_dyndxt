aadb2e11454ca6694ab61660f496402c06ec237a
Erik Abair <erik.abair@gmail.com>
2025-04-04 16:59:18 -0700

    Updates to support latest nxdk. Adds experimental handling of DMA subroutines.

--- devhost/src/pbkit_ext.cpp
+++ devhost/src/pbkit_ext.cpp
@@ -2,51 +2,7 @@
 
 #include <pbkit/pbkit.h>
 
-// clang format off
-#define _USE_MATH_DEFINES
-#include <cmath>
-// clang format on
-#include <vector>
-
 #include "debug_output.h"
-#include "nxdk_ext.h"
-
-void set_depth_stencil_buffer_region(uint32_t depth_buffer_format,
-                                     uint32_t depth_value,
-                                     uint8_t stencil_value, uint32_t left,
-                                     uint32_t top, uint32_t width,
-                                     uint32_t height) {
-  // See: pbkit.c: pb_erase_depth_stencil_buffer
-  uint32_t right = left + width;
-  uint32_t bottom = top + height;
-
-  auto p = pb_begin();
-
-  // sets rectangle coordinates
-  p = pb_push1(p, NV097_SET_CLEAR_RECT_HORIZONTAL,
-               ((right - 1) << 16) | (left & 0xFFFF));
-  p = pb_push1(p, NV097_SET_CLEAR_RECT_VERTICAL,
-               ((bottom - 1) << 16) | (top & 0xFFFF));
-
-  switch (depth_buffer_format) {
-    case NV097_SET_SURFACE_FORMAT_ZETA_Z16:
-      p = pb_push1(p, NV097_SET_ZSTENCIL_CLEAR_VALUE, depth_value & 0xFFFF);
-      break;
-
-    case NV097_SET_SURFACE_FORMAT_ZETA_Z24S8:
-      p = pb_push1(p, NV097_SET_ZSTENCIL_CLEAR_VALUE,
-                   ((depth_value & 0x00FFFFFF) << 8) | stencil_value);
-      break;
-
-    default:
-      ASSERT(!"Invalid depth_buffer_format");
-  }
-
-  p = pb_push1(p, NV097_CLEAR_SURFACE,
-               NV097_CLEAR_SURFACE_Z | NV097_CLEAR_SURFACE_STENCIL);
-
-  pb_end(p);
-}
 
 uint16_t float_to_z16(float val) {
   if (val == 0.0f) {
@@ -89,113 +45,79 @@ float z24_to_float(uint32_t val) {
   return *(float *)&val;
 }
 
-#define DMA_CLASS_3D 0x3D
-#define PB_SETOUTER 0xB2A
+void pb_print_with_floats(const char *format, ...) {
+  char buffer[512];
 
-// void pb_set_dma_address(const struct s_CtxDma *context, const void *address,
-// uint32_t limit) {
-//   uint32_t dma_addr = reinterpret_cast<uint32_t>(address) & 0x03FFFFFF;
-//   uint32_t dma_flags = DMA_CLASS_3D | 0x0000B000;
-//   dma_addr |= 3;
-//
-//   auto p = pb_begin();
-//   p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_WAIT_MAKESPACE, 0);
-//
-//   // set params addr,data
-//   p = pb_push2(p, NV20_TCL_PRIMITIVE_3D_PARAMETER_A, NV_PRAMIN +
-//   (context->Inst << 4) + 0x08, dma_addr);
-//
-//   // calls subprogID PB_SETOUTER: does VIDEOREG(addr)=data
-//   p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_FIRE_INTERRUPT, PB_SETOUTER);
-//   p = pb_push2(p, NV20_TCL_PRIMITIVE_3D_PARAMETER_A, NV_PRAMIN +
-//   (context->Inst << 4) + 0x0C, dma_addr); p = pb_push1(p,
-//   NV20_TCL_PRIMITIVE_3D_FIRE_INTERRUPT, PB_SETOUTER); p = pb_push2(p,
-//   NV20_TCL_PRIMITIVE_3D_PARAMETER_A, NV_PRAMIN + (context->Inst << 4) + 0x00,
-//   dma_flags); p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_FIRE_INTERRUPT,
-//   PB_SETOUTER); p = pb_push2(p, NV20_TCL_PRIMITIVE_3D_PARAMETER_A, NV_PRAMIN
-//   + (context->Inst << 4) + 0x04, limit); p = pb_push1(p,
-//   NV20_TCL_PRIMITIVE_3D_FIRE_INTERRUPT, PB_SETOUTER);
-//
-//   pb_end(p);
-// }
-//
-// void pb_bind_subchannel(uint32_t subchannel, const struct s_CtxDma *context)
-// {
-//   auto p = pb_begin();
-//   p = pb_push1_to(subchannel, p, NV20_TCL_PRIMITIVE_SET_MAIN_OBJECT,
-//   context->ChannelID); pb_end(p);
-// }
+  va_list argList;
+  va_start(argList, format);
+  vsnprintf_(buffer, 512, format, argList);
+  va_end(argList);
 
-void *pb_agp_access(void *fb_memory_pointer) {
-  return reinterpret_cast<void *>(
-      reinterpret_cast<uint32_t>(fb_memory_pointer) | AGP_MEMORY_REMAP);
+  char *str = buffer;
+  while (*str != 0) {
+    pb_print_char(*str++);
+  }
 }
 
-uint32_t *pb_push_4x3_matrix(uint32_t *p, DWORD command, const float *m) {
-  pb_push_to(SUBCH_3D, p++, command, 12);
+void pb_fetch_pgraph_registers(uint8_t *registers) {
+  // See
+  // https://github.com/XboxDev/nv2a-trace/blob/65bdd2369a5b216cfc47c9545f870c49d118276b/Trace.py#L32
 
-  *((float *)p++) = m[_11];
-  *((float *)p++) = m[_12];
-  *((float *)p++) = m[_13];
-  *((float *)p++) = m[_14];
+  while (pb_busy()) {
+    /* Wait for any pending pgraph commands to be flushed */
+  }
 
-  *((float *)p++) = m[_21];
-  *((float *)p++) = m[_22];
-  *((float *)p++) = m[_23];
-  *((float *)p++) = m[_24];
+  constexpr uint32_t kPGRAPHRegisterEnd =
+      PGRAPH_REGISTER_BASE + PGRAPH_REGISTER_ARRAY_SIZE;
 
-  *((float *)p++) = m[_31];
-  *((float *)p++) = m[_32];
-  *((float *)p++) = m[_33];
-  *((float *)p++) = m[_34];
+  struct Range {
+    uint32_t begin;
+    uint32_t end;
+  };
 
-  return p;
+  // clang-format off
+  static constexpr Range kReadRanges[] = {
+      {PGRAPH_REGISTER_BASE, 0xFD400200}, // From nv2a-trace: "0xFD400200 hangs Xbox, I just skipped to 0x400."
+      // Spotchecked 300, 360, 3B0, 3F0 also hang
+      {0xFD400400, 0xFD400754}, // From xemu, 0xFD400754 is the RDI interface.
+      {0xFD400758, kPGRAPHRegisterEnd},
+  };
+  // clang-format on
+
+  // Read a dword at a time as the target is MMIO.
+  uint32_t *last_empty = nullptr;
+  for (const auto &range : kReadRanges) {
+    auto src = reinterpret_cast<const uint32_t *>(range.begin);
+    auto end = reinterpret_cast<const uint32_t *>(range.end);
+    auto offset = range.begin - PGRAPH_REGISTER_BASE;
+    auto dst = reinterpret_cast<uint32_t *>(registers + offset);
+
+    while (last_empty && last_empty < dst) {
+      *last_empty++ = 0xBAD0BAD0;
+    }
+
+    while (src < end) {
+      if ((intptr_t)src >= 0xFD400200 && (intptr_t)src < 0xFD400400) {
+        PrintMsg("Checking 0x%p\n", src);
+        Sleep(10);
+      }
+      *dst++ = *src++;
+    }
+    last_empty = dst;
+  }
 }
 
-uint32_t *pb_push_4x4_matrix(uint32_t *p, DWORD command, const float *m) {
-  pb_push_to(SUBCH_3D, p++, command, 16);
+void pb_diff_registers(const uint8_t *a, const uint8_t *b,
+                       std::list<uint32_t> &modified_registers) {
+  modified_registers.clear();
 
-  *((float *)p++) = m[_11];
-  *((float *)p++) = m[_12];
-  *((float *)p++) = m[_13];
-  *((float *)p++) = m[_14];
+  auto old_val = reinterpret_cast<const uint32_t *>(a);
+  auto new_val = reinterpret_cast<const uint32_t *>(b);
 
-  *((float *)p++) = m[_21];
-  *((float *)p++) = m[_22];
-  *((float *)p++) = m[_23];
-  *((float *)p++) = m[_24];
-
-  *((float *)p++) = m[_31];
-  *((float *)p++) = m[_32];
-  *((float *)p++) = m[_33];
-  *((float *)p++) = m[_34];
-
-  *((float *)p++) = m[_41];
-  *((float *)p++) = m[_42];
-  *((float *)p++) = m[_43];
-  *((float *)p++) = m[_44];
-
-  return p;
-}
-
-uint32_t *pb_push1f(uint32_t *p, DWORD command, float param1) {
-  pb_push_to(SUBCH_3D, p, command, 1);
-  *((float *)(p + 1)) = param1;
-  return p + 2;
-}
-
-uint32_t *pb_push2f(uint32_t *p, DWORD command, float param1, float param2) {
-  pb_push_to(SUBCH_3D, p, command, 2);
-  *((float *)(p + 1)) = param1;
-  *((float *)(p + 2)) = param2;
-  return p + 3;
-}
-
-uint32_t *pb_push3f(uint32_t *p, DWORD command, float param1, float param2,
-                    float param3) {
-  pb_push_to(SUBCH_3D, p, command, 3);
-  *((float *)(p + 1)) = param1;
-  *((float *)(p + 2)) = param2;
-  *((float *)(p + 3)) = param3;
-  return p + 4;
+  for (auto i = 0; i < PGRAPH_REGISTER_ARRAY_SIZE / 4;
+       ++i, ++old_val, ++new_val) {
+    if (*old_val != *new_val) {
+      modified_registers.push_back(PGRAPH_REGISTER_BASE + i * 4);
+    }
+  }
 }
--- src/tracelib/pushbuffer_command.c
+++ src/tracelib/pushbuffer_command.c
@@ -8,14 +8,17 @@
 
 // #define VERBOSE_DEBUG
 
-static const uint32_t kTag = 0x6E745043;  // 'ntPC'
+static const uint32_t kTag = 0x6E745043; // 'ntPC'
 
 // Offset that must be added to pushbuffer commands in order to read them.
 static const uint32_t kAccessibleAddrOffset = 0x80000000;
 #define PB_ADDR(a) (kAccessibleAddrOffset | (a))
 
-uint32_t ParsePushBufferCommand(uint32_t addr, uint32_t command,
-                                PushBufferCommand *info) {
+uint32_t ParsePushBufferCommand(uint32_t addr,
+                                uint32_t command,
+                                PushBufferCommandTraceInfo *trace) {
+  PushBufferCommand *info = &trace->command;
+
 #ifdef VERBOSE_DEBUG
   char buf[128];
   int buf_end = sprintf(buf, "0x%08X: Opcode: 0x%08X", addr, command);
@@ -44,10 +47,26 @@ uint32_t ParsePushBufferCommand(uint32_t addr, uint32_t command,
   }
 
   if ((command & 3) == 2) {
+    addr += 4;
+    uint32_t subroutine_addr = command & 0xFFFFFFFC;
 #ifdef VERBOSE_DEBUG
-    sprintf(buf + buf_end, "; unhandled opcode type: call\n");
+    sprintf(buf + buf_end, "; call 0x%08X - return to 0x%08X\n", subroutine_addr, addr);
     DbgPrint(buf);
 #endif  // VERBOSE_DEBUG
+    if (trace->subroutine_return_address) {
+#ifdef VERBOSE_DEBUG
+      sprintf(buf + buf_end,
+          "; call 0x%08X - return to 0x%08X but nested in subroutine with return 0x%08X\n",
+          subroutine_addr,
+          addr,
+          trace->subroutine_return_address
+      );
+      DbgPrint(buf);
+#endif  // VERBOSE_DEBUG
+      return 0;
+    }
+
+    trace->subroutine_return_address = addr;
     // if (state->subroutine_active) {
     //  state->error = NV_PFIFO_CACHE1_DMA_STATE_ERROR_CALL;
     //  break;
@@ -55,7 +74,7 @@ uint32_t ParsePushBufferCommand(uint32_t addr, uint32_t command,
     // state->subroutine_return = control->dma_get;
     // state->subroutine_active = true;
     // control->dma_get = command & 0xfffffffc;
-    return 0;
+    return subroutine_addr;
   }
 
   if (command == 0x00020000) {
@@ -63,7 +82,22 @@ uint32_t ParsePushBufferCommand(uint32_t addr, uint32_t command,
     sprintf(buf + buf_end, "; unhandled opcode type: return\n");
     DbgPrint(buf);
 #endif  // VERBOSE_DEBUG
-    return 0;
+    if (!trace->subroutine_return_address) {
+#ifdef VERBOSE_DEBUG
+      sprintf(buf + buf_end,
+          "; call 0x%08X - return to 0x%08X but nested in subroutine with return 0x%08X\n",
+          subroutine_addr,
+          addr,
+          trace->subroutine_return_address
+      );
+      DbgPrint(buf);
+#endif  // VERBOSE_DEBUG
+      return 0;
+    }
+
+    addr = trace->subroutine_return_address;
+    trace->subroutine_return_address = 0;
+    return addr;
   }
 
   uint32_t masked = command & 0xE0030003;
@@ -91,10 +125,11 @@ uint32_t ParsePushBufferCommand(uint32_t addr, uint32_t command,
   return addr;
 }
 
-static BOOL ReadParameters(uint32_t pull_addr, uint32_t count,
+static BOOL ReadParameters(uint32_t pull_addr,
+                           uint32_t count,
                            PushBufferCommandParameters *data) {
   uint32_t data_len = count * 4;
-  const uint8_t *data_addr = (const uint8_t *)(PB_ADDR(pull_addr));
+  const uint8_t *data_addr = (const uint8_t *) (PB_ADDR(pull_addr));
   data_addr += 4;
 
   if (data_len <= sizeof(data->data.buffer)) {
@@ -103,11 +138,12 @@ static BOOL ReadParameters(uint32_t pull_addr, uint32_t count,
     return TRUE;
   }
 
-  data->data.heap_buffer = (uint8_t *)DmAllocatePoolWithTag(data_len, kTag);
+  data->data.heap_buffer = (uint8_t *) DmAllocatePoolWithTag(data_len, kTag);
   if (!data->data.heap_buffer) {
     DbgPrint(
-        "Allocation failed processing %d data bytes for command at 0x%08X\n",
-        data_len, pull_addr);
+      "Allocation failed processing %d data bytes for command at 0x%08X\n",
+      data_len,
+      pull_addr);
     data->data_state = PBCPDS_INVALID;
     return FALSE;
   }
@@ -131,11 +167,12 @@ uint32_t ParsePushBufferCommandTraceInfo(uint32_t pull_addr,
 
   // FIXME: Get where this command ends.
   uint32_t next_parser_addr =
-      ParsePushBufferCommand(pull_addr, raw_cmd, &info->command);
+      ParsePushBufferCommand(pull_addr, raw_cmd, info);
 
   if (!next_parser_addr) {
     // If we don't know where this command ends, we have to abort.
-    DbgPrint("Failed to process command 0x%08X at 0x%08X\n", raw_cmd,
+    DbgPrint("Failed to process command 0x%08X at 0x%08X\n",
+             raw_cmd,
              pull_addr);
     return 0;
   }
@@ -153,7 +190,8 @@ uint32_t ParsePushBufferCommandTraceInfo(uint32_t pull_addr,
     // Note: Halo: CE has cases where `parameter_count` == 0 that must be
     // accounted for.
     if (info->command.parameter_count && !discard_parameters) {
-      if (!ReadParameters(pull_addr, info->command.parameter_count,
+      if (!ReadParameters(pull_addr,
+                          info->command.parameter_count,
                           &info->data)) {
         info->valid = FALSE;
         return 0;
@@ -166,21 +204,23 @@ uint32_t ParsePushBufferCommandTraceInfo(uint32_t pull_addr,
   return next_parser_addr;
 }
 
-BOOL GetParameter(const PushBufferCommandTraceInfo *info, uint32_t index,
+BOOL GetParameter(const PushBufferCommandTraceInfo *info,
+                  uint32_t index,
                   uint32_t *out) {
   if (!info || !info->valid || info->data.data_state == PBCPDS_INVALID) {
     return FALSE;
   }
 
   if (index >= info->command.parameter_count) {
-    DbgPrint("GetParameter: Index out of range (%u >= %u)\n", index,
+    DbgPrint("GetParameter: Index out of range (%u >= %u)\n",
+             index,
              info->command.parameter_count);
     return FALSE;
   }
 
   const uint32_t *data = info->data.data_state == PBCPDS_HEAP_BUFFER
-                             ? (const uint32_t *)info->data.data.heap_buffer
-                             : info->data.data.buffer;
+                           ? (const uint32_t *) info->data.data.heap_buffer
+                           : info->data.data.buffer;
   *out = data[index];
 
   return TRUE;
